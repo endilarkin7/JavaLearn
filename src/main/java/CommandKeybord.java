72844db799784fdbc773c3ddb5fe6d3fa62e8268
VitaliyTyshko <vetal.t28@gmail.com>
2018-11-07 03:23:13 +0200

    Initial commit

---
+++ src/main/java/CommandKeybord.java
@@ -0,0 +1,14 @@
+public class CommandKeybord {
+
+    //    shift+enter
+//    psvm (entry point) = public static void main(String[] args) { }
+//    ctrl + d = duplicate code
+//    ctrl + y = delete string (строку)
+//    ctrl + shift + z  => return in 1 step forward(вперёд)
+//    ctrl + alt + L   => reformat code
+//    ctrl + /  =≥ comment
+//    ctrl + shift + /  =≥ comment
+//    shift + F6 - rename Class,variables in all code
+//    i.sout = System.out.println(i);
+//    JCC = Java Code Convetion
+}
---
+++ src/main/java/lesson3.java
@@ -0,0 +1,68 @@
+public class lesson3 {
+
+    //constant(const число)
+
+    final byte AB = 2 ;  //final - константа, где АB - большая
+//
+
+
+    public class HelloWorld {
+
+
+
+
+
+
+
+    }
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 5;
+
+
+            System.out.println(10 >> 3);
+            System.out.println(10 << 3);
+
+            System.out.println(-1 >>> 63);
+
+//        System.out.println(10 >>> 2);
+
+//        if (a < b || 10 > 5 / 0) {
+//            System.out.println(a);
+//        } else {
+//            System.out.println(b);
+//        }
+
+//        int i_2 = 0b1010; // B
+//        int i_8 = 010;
+//        int i_10 = 10;
+//        int i_16 = 0xA; // X
+
+
+
+//        System.out.println(i_2);
+//        byte a = 10;
+//        byte b = 20;
+
+            // Compile Error
+//        byte c = (byte) a + b;
+
+            // Runtime Error
+//        System.out.println(10 / 0);
+
+//        byte dollar = (byte) 130; // -128...127
+//
+//        System.out.println(dollar);
+//
+//        long i = 2_147_483_6471L;
+//
+//        System.out.println(Integer.MAX_VALUE);
+
+//        byte b = 100;
+//
+//        int i = b;
+//
+//        byte c = (byte) i;
+
+        }
+    }
